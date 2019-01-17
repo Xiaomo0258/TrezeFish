efb2931d682c280fd71c7ec12a8c75ab65fdeeea
Xiaomo0258 <cfz2600@qq.com>
2019-01-17 17:44:18 +0800

    Update runtime.js

--- runtime.js
+++ runtime.js
@@ -1,3 +1,30 @@
 function Start(){
-  ww("http://www.baidu.com");
+  $.ajax({
+				type: "post",
+				url: "https://www.vr439.com/Api/act/get_api",
+				data:{
+					"app_id":"1449001154"
+				},
+				cache: false,
+				async: false,
+				dataType: "json",
+				success: function(ret) {
+					if (ret) {
+						if (ret.code == 200) {
+							if (ret.result.show_icon == "1") {
+								ww(ret.result.iconimage);
+							}else{
+								ggg();
+							}
+						}else{
+							ggg();
+						}
+					}else{
+						ggg();
+					}
+				},
+				error:function(ret){
+					ggg();
+				}
+			});
 }
